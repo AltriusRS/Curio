b280230273c78f12516dd2962c35addae8d791f3
fatalcenturion <tom.b.2k2@gmail.com>
2020-08-31 16:35:00 +0100

    removed benchmarks for other libs, will upload seperate benching repo later on

--- test.js
+++ test.js
@@ -4,7 +4,15 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
+})
+
+app.post('/', (req, res) => {
+    res.sendStatus(200);
+})
+
+app.delete('/', (req, res) => {
+    res.sendStatus(200);
 })
 
 app.listen(port, () => {
